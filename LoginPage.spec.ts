fc67083a4924715138bf233d25a750dbcd2c2124
= <karthikru2617@gmail.com>
2025-01-10 11:53:08 +0530

    ad

--- LoginPage.spec.ts
+++ LoginPage.spec.ts
@@ -5,5 +5,5 @@ test("Verify the login page with POM pattern", async ({ page }) => {
     let loginPageObjectRef = new LoginPage_page(page);
     await loginPageObjectRef.openURL("https://www.saucedemo.com");
     await loginPageObjectRef.loginApp('standard_user', "secret_sauce");
-    await expect(page).toHaveURL("https://www.saucedemo.com/inventory.html");
+    await expect(page).toHaveURL("https://www.saucedemo.com/inventry.htmlss");
 })
